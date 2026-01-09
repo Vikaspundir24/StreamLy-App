b6cf1b0f77777417c648ece4bd6f871158fe2a92
Vikas Pundir <vikaspundiruser@gmail.com>
2026-01-10 03:14:28 +0530

    express configured

--- src/index.js
+++ src/index.js
@@ -12,6 +12,19 @@ dotenv.config({
 
 
 connectDB()
+.then(()=> {
+    app.on("error", (error) => {
+        console.log("ERR : ",error);
+        throw error
+
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+console.log("MONGO db Connection Failed!!" , err);
+})
 
 
 
