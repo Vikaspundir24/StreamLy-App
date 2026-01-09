593162f10f0d7cf4b49fba4d0879b119d87e013d
Vikas Pundir <vikaspundiruser@gmail.com>
2026-01-10 04:19:14 +0530

    util code added

--- src/app.js
+++ src/app.js
@@ -10,10 +10,10 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit:"16kb"})) //to limit the json input
+app.use(express.urlencoded({extended:true, limit:"16kb"}))  //encoding in URL eg..space= %20
+app.use(express.static("public")) //If someone requests a file, first check the public folder and serve it directly
+app.use(cookieParser()) //Used to parse cookies from incoming requests and make them accessible via req.cookies.
 
 
 
