1a5df4c5078aa4198d692ad6158a468fa600f9a6
Vikas Pundir <vikaspundiruser@gmail.com>
2026-01-13 07:38:27 +0530

    tokens done

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema({
         trim: true,
         
     },
-    fullname:{
+    fullName:{
         type:String,
         required: true,
         trim: true,
@@ -56,11 +56,40 @@ userSchema.pre("save", async function(next) {
     if(!this.isModified("password")){
         return next();
     }
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
 userSchema.methods.isPasswordCorrect = async function (password){
    return await bcrypt.compare(password,this.password)
 } 
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id : this._id,
+        email : this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRTY
+    }
+)
+}
+
+
+userSchema.methods.generateRefreshToken = function(){
+     return jwt.sign({
+        _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRTY
+    }
+)
+}
+
+
+
 export const User = mongoose.model("User",userSchema)
