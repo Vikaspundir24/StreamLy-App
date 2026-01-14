e4f860003be42478bf540fe7491a8758cebbba9a
Vikas Pundir <vikaspundiruser@gmail.com>
2026-01-15 05:22:48 +0530

    multer added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,1 +1,13 @@
 import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+   /* const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) */
+    cb(null, file.originalname) // needs to be updated in further upgrade as it can have multiple same file names..tho it will be short period of time in server but still..
+  }
+})
+
+export const upload = multer({ storage, })
